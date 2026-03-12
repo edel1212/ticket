92af537718011d5a6bf496b3b8c636c1788cb7a0
yoo <edel1212@naver.com>
2026-03-12 17:58:46 +0900

    BaseTimeEntity 추가

---
+++ ticket-server/src/main/java/com/yoo/ticket/global/common/entity/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.yoo.ticket.global.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.Comment;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    @Comment("등록 날짜")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Comment("수정 날짜")
+    private LocalDateTime modifiedAt;
+}
